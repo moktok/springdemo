d88084e4202289448cf2843539c5610292fe7544
Patrick Geudens <Patrick.Geudens@realdolmen.com>
2022-11-04 11:44:17 +0100

    The very last commit, :(

--- src/test/java/be/infernalwhale/springdemo/service/TicketServiceImpl_IntegrationTest.java
+++ src/test/java/be/infernalwhale/springdemo/service/TicketServiceImpl_IntegrationTest.java
@@ -3,22 +3,38 @@ package be.infernalwhale.springdemo.service;
 import be.infernalwhale.springdemo.db.Price;
 import be.infernalwhale.springdemo.db.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.TestPropertySources;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@TestPropertySource("classpath:application-integrationtest.properties")
+@TestPropertySources([
+        @TestPropertySource("classpath:application-integrationtest.properties")])
         // TestPropertySource; Load application properties without creating/loading a profile
         // TestPropertySources for multiple sources, order matters
+    @Profile({"prof1", "prof2"})
+@DataJpaTest
+@WebMvcTest
 class TicketServiceImpl_IntegrationTest {
+
+    @MockBean
+    MockMvc mockMvc;
+
+    @Test
+    void mvcTest() {
+
+    }
 
     @TestConfiguration
     static class EmployeeServiceImplTestContextConfiguration {
@@ -34,7 +50,8 @@ class TicketServiceImpl_IntegrationTest {
     }
 
     @MockBean
-    private ProductRepository productRepository; // Will behave like normal mock.. but Spring will be aware of this bean
+    private ProductRepository productRepository;
+    // Will behave like normal mock.. but Spring will be aware of this bean
 
     @Test
     public void contextLoads() {
